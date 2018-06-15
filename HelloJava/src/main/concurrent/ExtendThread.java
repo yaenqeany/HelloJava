090631e4a3fb2a256da6bbb7ca2e6c2063458e2c
wulong.liu <yaenqeany@sina.com>
2018-06-15 18:41:45 +0800

    Java base-- thread

--- HelloJava/src/main/concurrent/ExtendThread.java
+++ HelloJava/src/main/concurrent/ExtendThread.java
@@ -1,4 +1,20 @@
 package main.concurrent;
 
-public class ExtendThread {
+public class ExtendThread extends Thread {
+    private String name;
+
+    public ExtendThread(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + " is running " + i);
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
