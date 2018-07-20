31df2d56909ca2538709d8b0348bf3632e3add42
yaenqeany <yaenqeany@sina.com>
2018-07-20 17:30:23 +0800

    add io demo

--- HelloJava/src/main/file_io/IoDemo.java
+++ HelloJava/src/main/file_io/IoDemo.java
@@ -12,53 +12,93 @@ public class IoDemo {
                 e.printStackTrace();
             }
         }
-        OutputStream outputStream = null;
-        BufferedOutputStream bos = null;
+        IoDemo ioDemo = new IoDemo();
+//        ioDemo.fileReadByStresm(file);
+        ioDemo.fileReadByReader(file);
+    }
 
-        Writer writer = null;
-        BufferedWriter bw = null;
-//        try {
-//            outputStream = new FileOutputStream(file);
-////            outputStream.write("abc".getBytes());
-//
-//            bos = new BufferedOutputStream(outputStream);
-//            bos.write("vnm".getBytes());
-//
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-////                outputStream.flush();
-////                outputStream.close();
-//
-//                bos.flush();
-//                bos.close();
-//
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-        try {
-            writer = new FileWriter(file);
-//            writer.write("sadfjlsajflkajfklsdafdlk");
-            bw = new BufferedWriter(writer);
-            bw.newLine();
-            bw.append("BufferedWriter");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            //                writer.flush();
-//                writer.close();
-
+    public void fileReadByStresm(File file) {
+        if (null == file || !file.isFile()) {
+            System.out.println("parameter error : file is null or not a file!");
+        } else {
+            InputStream inputStream = null;
+            BufferedInputStream bis = null;
             try {
-                bw.flush();
-                bw.close();
+                inputStream = new FileInputStream(file);
+                bis = new BufferedInputStream(inputStream);
+                byte[] bytes = new byte[12];//读取结果数组容器
+                while (true) {
+                    int result = inputStream.read();
+                    if (-1 != result) {
+                        System.out.println((char) result);
+                    } else {
+                        break;
+                    }
+                    /*int result = inputStream.read(bytes);
+                    if (-1 != result) {
+                        for (int i = 0; i < result; i++) {
+                            System.out.println((char) bytes[i]);
+                        }
+                    } else {
+                        break;
+                    }*/
+/*//                    result = bis.read();
+                    result = bis.read(bytes);
+                    if (-1 != result) {
+                        System.out.println((char) result);
+                        System.out.println("===========");
+                    } else {
+                        break;
+                    }*/
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void fileReadByReader(File file) {
+        if (null == file || !file.isFile()) {
+            System.out.println("parameter error : file is null or not a file!");
+        } else {
+            Reader reader = null;
+            BufferedReader br = null;
+            try {
+                reader = new FileReader(file);
+                br = new BufferedReader(reader);
+                while (true) {
+                    /*int result = reader.read();
+                    if (-1 != result) {
+                        System.out.println((char)result);
+                    }*/
+                    String result = br.readLine();
+                    if (null != result && "" != result) {
+                        System.out.println(result);
+                    } else {
+                        break;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    reader.close();
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
